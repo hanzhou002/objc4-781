f74754b8abda9bc919654ee9963d26de187f813f
hao xiangfeng <hanzhou002@163.com>
2020-09-14 00:49:20 +0800

    objc4-781源码编译成功

---
+++ Detector/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Detector
+//
+//  Created by 郝相峰 on 2020/9/13.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
